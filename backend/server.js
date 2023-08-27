2cdb0998fccd3d3dddb6b9b95e680ce0fd15b697
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-08-27 21:14:45 +0530

    Frontend and backend created

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {chats} = require("./data/data.js");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/",(req,res) =>{
+    res.send("API is running")
+})
+
+app.get("/api/chat",(req,res) =>{
+    res.send("chats");
+});
+
+app.get("/api/chat/:id",(req,res) =>{
+    console.log(req)
+    const singleChat = chats.find((c)=> c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`Server started on port ${PORT}`));
