7e6cfc71e42b7dcffe1364f2d2910fdfec315263
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-08-30 02:44:22 +0530

    completed all Chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  removeFromGroup,
+  addToGroup,
+  renameGroup,
+} = require("../controllers/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats }= require("../controllers/chatControllers")
 
 const router = express.Router();
 
-router.route("/").post(protect, accessChat); //checked
-router.route("/").get(protect, fetchChats);
-// router.route("/group").get(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/").post(protect, accessChat); //working
+router.route("/").get(protect, fetchChats); //not working 
+router.route("/group").post(protect, createGroupChat); //working
+router.route("/rename").put(protect, renameGroup); //working
+router.route("/groupremove").put(protect, removeFromGroup); //working
+router.route("/groupadd").put(protect, addToGroup); //working
 
 module.exports = router;
